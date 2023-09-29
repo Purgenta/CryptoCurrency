51472d8535b836c0f60b2218ede4216be32ecb8a
Purgenta <onlinenidza352@gmail.com>
2023-09-29 18:02:27 +0200

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Router from "./router/Router";
+
+const App = () => {
+  return <Router></Router>;
+};
+
+export default App;
---
+++ src/router/Router.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+const Router = () => {
+  return <Routes></Routes>;
+};
+
+export default Router;
