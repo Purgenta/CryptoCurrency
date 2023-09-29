7ea663ba14a5afdc412d347ced971296f5773e8b
Purgenta <onlinenidza352@gmail.com>
2023-09-29 19:23:46 +0200

    Added application layout and simple animations

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Layout from "../components/Layout/Layout";
 const Router = () => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<></>}></Route>
+        <Route path="/home" element={<></>}></Route>
+        <Route path="/favourites" element={<></>}></Route>
+        <Route path="/details" element={<></>}></Route>
+      </Route>
+    </Routes>
+  );
 };
 
 export default Router;
