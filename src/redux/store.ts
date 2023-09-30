ef90c9d4d2ef4c9e30ec7560288c01bc09f1d86c
Purgenta <onlinenidza352@gmail.com>
2023-09-30 19:39:45 +0200

    Added proxy server, added base functionality for details along with favourite store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice/authSlice";
 import { currenciesApi } from "./api/apiSlice";
+import { currencyDetailsApi } from "./api/detailsSlice";
+import favouriteSlice from "./favouriteSlice/favouriteSlice";
 const store = configureStore({
   reducer: {
     auth: authSlice,
     [currenciesApi.reducerPath]: currenciesApi.reducer,
+    [currencyDetailsApi.reducerPath]: currencyDetailsApi.reducer,
+    favourites: favouriteSlice,
   },
-  middleware: (defMiddleware) => {
-    return defMiddleware().concat(currenciesApi.middleware);
-  },
+  middleware: (getdefaultMiddleware) =>
+    getdefaultMiddleware().concat([
+      currenciesApi.middleware,
+      currencyDetailsApi.middleware,
+    ]),
 });
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
