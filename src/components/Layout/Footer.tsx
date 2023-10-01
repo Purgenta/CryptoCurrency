6bf0cfb10dc37e3d34519618b49e43d5d37da350
Purgenta <onlinenidza352@gmail.com>
2023-10-01 17:27:49 +0200

    Fixed an issue with react import during build, added readme

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import style from "./Footer.module.css";
 import Currencies from "../Currencies/Currencies";
 const Footer = () => {
--- src/components/Suspense/Loader.tsx
+++ src/components/Suspense/Loader.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import style from "./Loader.module.css";
 import { ClipLoader } from "react-spinners";
 const Loader = () => {
